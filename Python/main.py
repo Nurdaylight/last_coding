c8d79fde990f425199a27be660891a08ee4ba34c
Nurdaylight <157308256+Nurdaylight@users.noreply.github.com>
2024-03-01 21:23:33 +0100

    Adding for loop

--- Python/main.py
+++ Python/main.py
@@ -1,1 +1,10 @@
 
+#lets say we have some list:
+list= [8,6,5,1,2,3,5,9,5,6,7,8,9,0,1,2,3,4,5,6,7]
+#imagine we want to print out all the odd values in the list
+for i in list:
+  # Check if the value is odd
+  if i % 2 != 0:
+      print(i)
+
+
